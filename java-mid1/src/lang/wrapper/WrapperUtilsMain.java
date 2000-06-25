c426f8f52dec17617c0330f3f9aedbd8cdd9b4f8
박주찬 <p990805@naver.com>
2000-06-25 10:10:00 +0000

    [주찬] Docs : 중급 자바 섹션5 wrapper 수강

--- java-mid1/src/lang/wrapper/WrapperUtilsMain.java
+++ java-mid1/src/lang/wrapper/WrapperUtilsMain.java
@@ -2,15 +2,16 @@ package lang.wrapper;
 
 public class WrapperUtilsMain {
     public static void main(String[] args) {
-        Integer i1 =Integer.valueOf(10);
-        Integer i2 = Integer.valueOf("10");
-        int intValue = Integer.parseInt("10");
+        Integer i1 =Integer.valueOf(10); // 숫자, 래퍼 객체 반환
+        Integer i2 = Integer.valueOf("10"); // 문자열, 래퍼 객체 반환
+        int intValue = Integer.parseInt("10"); // 문자열 전용, 기본형 반환
 
+        // 비교
         int compareResult = i1.compareTo(20);
-        System.out.println("compareREsult = " +  compareResult);
+        System.out.println("compareResult = " +  compareResult); //-1
 
-        System.out.println("sum = " + Integer.sum(10,20));
-        System.out.println("min = " + Integer.min(10,20));
-        System.out.println("max = " + Integer.max(10,20));
+        System.out.println("sum = " + Integer.sum(10,20)); // 30
+        System.out.println("min = " + Integer.min(10,20)); // 10
+        System.out.println("max = " + Integer.max(10,20)); // 20
     }
 }
