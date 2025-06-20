9728888ce03a06cb28a87c46f3b3e900ad25a3d8
박주찬 <p990805@naver.com>
2025-06-20 12:01:45 +0900

    [주찬] Docs : 중급 자바 섹션4 String 클래스 수강 끝

---
+++ java-mid1/src/lang/string/chaining/MethodChainingMain1.java
@@ -0,0 +1,12 @@
+package lang.string.chaining;
+
+public class MethodChainingMain1 {
+    public static void main(String[] args) {
+        ValueAdder adder = new ValueAdder();
+        ValueAdder adder1 = adder.add(1);
+        ValueAdder adder2 = adder.add(2);
+        ValueAdder adder3 = adder.add(3);
+        int result = adder.getValue();
+        System.out.println("result : " + result);
+    }
+}
---
+++ java-mid1/src/lang/string/method/StringChangeMain1.java
@@ -0,0 +1,12 @@
+package lang.string.method;
+
+public class StringChangeMain1 {
+    public static void main(String[] args) {
+        String str = "Hello, Java! Welcome to Java";
+        System.out.println("인덱스 7부터의 부분 문자열: " + str.substring(7));
+        System.out.println("인덱스 7부터 12까지의 부분 문자열: "+ str.substring(7,12));
+        System.out.println("문자열 결합: " + str.concat("!!!"));
+        System.out.println("java를 world로 대체 " + str.replace("Java", "World"));
+        System.out.println("첫번째 java를 world으로 대체 " + str.replaceFirst("Java", "World"));
+    }
+}
